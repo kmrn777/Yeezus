efb47abdb00d67c569ad6ee7dbd1fba06918bbd0
KMRN <kmrnmain@gmail.com>
2022-06-05 20:01:03 +0200

    Yeezus

---
+++ app/src/main/java/com/marsad/catchy/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.marsad.catchy.utils;
+
+public class Constants { //best way to keep constants in android
+
+    public static final String PREF_NAME = "pref_name", PREF_STORED = "pref_store",
+            PREF_URL = "pref_url", PREF_DIRECTORY = "pref_direct";
+
+}
